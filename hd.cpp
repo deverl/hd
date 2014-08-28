696be02a3869ef7bf2f7abfa1ca750a229802297
DeVerl Stokes <deverl.stokes@gmail.com>
2014-08-28 07:41:41 -0600

    Formatted with astyle.

--- hd.cpp
+++ hd.cpp
@@ -27,8 +27,8 @@
 
 
 
-const unsigned int MAX_READ_BUF_SIZE( 65536 );
-const unsigned int STRSIZE( 128 );
+const unsigned int MAX_READ_BUF_SIZE(65536);
+const unsigned int STRSIZE(128);
 
 
 // Display flags.
@@ -42,20 +42,20 @@ enum DisplayFlags
 };
 
 
-inline char AsciiFromBin( unsigned char b )
+inline char AsciiFromBin(unsigned char b)
 {
     // return (char)( ( b > 0x1f && b < 0x80 ) ? (char) b : '.' );
-    return (char)( ( b > 0x1f && b < 0x7f ) ? (char) b : '.' );
+    return (char)((b > 0x1f && b < 0x7f) ? (char) b : '.');
 }
 
 
-inline unsigned short hi_word( unsigned int i )
+inline unsigned short hi_word(unsigned int i)
 {
-    return ( i >> 16 ) & 0x0000ffff;
+    return (i >> 16) & 0x0000ffff;
 }
 
 
-inline unsigned short lo_word( unsigned int i )
+inline unsigned short lo_word(unsigned int i)
 {
     return i & 0x0000ffff;
 }
@@ -69,14 +69,14 @@ struct GLOBAL
 {
 public:
     GLOBAL()
-      : num_args( 0 ),
-        num_bytes( 0 ),
-        offset( 0 ),
-        use_num_bytes( false ),
-        use_offset( false )
+        : num_args(0),
+          num_bytes(0),
+          offset(0),
+          use_num_bytes(false),
+          use_offset(false)
     {
     }
-    
+
 public:
     int num_args;
     int num_bytes;
@@ -115,106 +115,106 @@ const char *usage[] =
 
 // Function prototypes.
 
-bool hex_dump_file( char const *path, unsigned int flags );
+bool hex_dump_file(char const *path, unsigned int flags);
 
-char *format_hex( char *str,  unsigned int flags,
-                  unsigned int offset, unsigned char const *buffer, unsigned int count );
+char *format_hex(char *str,  unsigned int flags,
+                 unsigned int offset, unsigned char const *buffer, unsigned int count);
 
 #ifdef _FILELENGTH
-unsigned int file_length( FILE *fp );
+unsigned int file_length(FILE *fp);
 #endif
 
-void display_text( const char **str );
+void display_text(const char **str);
 
 
 
 
 
 
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
     int result(0);
     int opt;
-    unsigned int flags( DF_SHOW_ADDRESS | DF_SHOW_ASCII | DF_PAGE_ALIGN );
+    unsigned int flags(DF_SHOW_ADDRESS | DF_SHOW_ASCII | DF_PAGE_ALIGN);
 
     // Process command line arguments.
-    
+
     do
     {
-        const char *optstring( "aAhkn:s:" );
+        const char *optstring("aAhkn:s:");
 
-        opt = getopt( argc, argv, optstring );
-        
-        switch( opt )
+        opt = getopt(argc, argv, optstring);
+
+        switch(opt)
         {
-            case 'a':
-                flags &= ~DF_SHOW_ADDRESS;
-                DBGCODE( printf( "Turning off address display\n" ); )
-                break;
-                
-            case 'A':
-                flags &= ~DF_SHOW_ASCII;
-                DBGCODE( printf( "Turning off ASCII display\n" ); )
-                break;
-                
-            case 'h':
-                display_text( usage );
-                exit( 0 );
-                break;
-                
-            case 'k':
-                flags &= ~DF_PAGE_ALIGN;
-                DBGCODE( printf( "Turning off page alignment\n" ); )
-                break;
-                
-            case 'n':
-                global.num_bytes = strtoul( optarg, NULL, 16 );
-                global.use_num_bytes = true;
-                DBGCODE( printf( "Got -n argument.  Number of bytes to dump: %u\n", global.num_bytes ); )
-                break;
-                
-            case 's':
-                global.offset = strtoul( optarg, NULL, 16 );
-                global.use_offset = true;
-                DBGCODE( printf( "Got -o argument.  Will dump from offset: %u\n", global.offset ); )
-                break;
-                
-            case '?':  // Unrecognized arguments.
-            default:
-                break;
-        }
-        
-    }
-    while( opt != EOF );
+        case 'a':
+            flags &= ~DF_SHOW_ADDRESS;
+            DBGCODE(printf("Turning off address display\n");)
+            break;
 
-    DBGCODE( printf( "optind = %d, opterr = %d, optopt = %d\n", optind, opterr, optopt ); )
-    
-    const int num_args( argc - optind );
+        case 'A':
+            flags &= ~DF_SHOW_ASCII;
+            DBGCODE(printf("Turning off ASCII display\n");)
+            break;
+
+        case 'h':
+            display_text(usage);
+            exit(0);
+            break;
+
+        case 'k':
+            flags &= ~DF_PAGE_ALIGN;
+            DBGCODE(printf("Turning off page alignment\n");)
+            break;
+
+        case 'n':
+            global.num_bytes = strtoul(optarg, NULL, 16);
+            global.use_num_bytes = true;
+            DBGCODE(printf("Got -n argument.  Number of bytes to dump: %u\n", global.num_bytes);)
+            break;
+
+        case 's':
+            global.offset = strtoul(optarg, NULL, 16);
+            global.use_offset = true;
+            DBGCODE(printf("Got -o argument.  Will dump from offset: %u\n", global.offset);)
+            break;
+
+        case '?':  // Unrecognized arguments.
+        default:
+            break;
+        }
+
+    }
+    while(opt != EOF);
+
+    DBGCODE(printf("optind = %d, opterr = %d, optopt = %d\n", optind, opterr, optopt);)
+
+    const int num_args(argc - optind);
 
     global.num_args = num_args;
-    
-    DBGCODE( printf( "num_args = %d\n", num_args ); )
-    
-    if( num_args > 0 )
+
+    DBGCODE(printf("num_args = %d\n", num_args);)
+
+    if(num_args > 0)
     {
         char *arg;
-        
+
         argv++;     // Step past the program name.
-        
-        while( ( arg = *argv++ ) != 0 )
+
+        while((arg = *argv++) != 0)
         {
-            DBGCODE( printf( "Calling hex_dump_file( %s )\n", arg ) ; )
-            
-            if ( num_args > 1 )
+            DBGCODE(printf("Calling hex_dump_file( %s )\n", arg) ;)
+
+            if(num_args > 1)
             {
-                printf( "%s\n", arg );
+                printf("%s\n", arg);
             }
-            
-            result += hex_dump_file( arg, flags );
-            
-            if ( num_args > 1 )
+
+            result += hex_dump_file(arg, flags);
+
+            if(num_args > 1)
             {
-                printf( "\n" );
+                printf("\n");
             }
         }
     }
@@ -222,7 +222,7 @@ int main( int argc, char **argv )
     {
         result = -1;
     }
-    
+
     return result;
 }
 
@@ -230,16 +230,16 @@ int main( int argc, char **argv )
 
 
 
-bool hex_dump_file( char const *path, unsigned int flags )
+bool hex_dump_file(char const *path, unsigned int flags)
 {
-    FILE *fp( fopen( path, "rb" ) );
-    
-    if( fp != 0 )
+    FILE *fp(fopen(path, "rb"));
+
+    if(fp != 0)
     {
-        unsigned int size( file_length( fp ) );
+        unsigned int size(file_length(fp));
         unsigned int bytes_to_dump;
-        
-        if( global.use_num_bytes )
+
+        if(global.use_num_bytes)
         {
             bytes_to_dump = global.num_bytes;
         }
@@ -247,93 +247,93 @@ bool hex_dump_file( char const *path, unsigned int flags )
         {
             bytes_to_dump = size;
         }
-        
-        unsigned int buffer_size( std::min( MAX_READ_BUF_SIZE, bytes_to_dump ) );
-        
-        unsigned char *buffer( new unsigned char[ buffer_size ] );
-        
-        if( buffer != 0 )
+
+        unsigned int buffer_size(std::min(MAX_READ_BUF_SIZE, bytes_to_dump));
+
+        unsigned char *buffer(new unsigned char[ buffer_size ]);
+
+        if(buffer != 0)
         {
-            char *str( new char[ STRSIZE ] );
-            
-            if( str != 0 )
+            char *str(new char[ STRSIZE ]);
+
+            if(str != 0)
             {
                 int offset;
-                
+
                 offset = global.use_offset ? global.offset : 0L;
-                
-                fseek( fp, offset, SEEK_SET );
-                
-                DBGCODE( printf( "Starting at offset: %u\n", offset ); )
-                
-                while( bytes_to_dump > 0 )
+
+                fseek(fp, offset, SEEK_SET);
+
+                DBGCODE(printf("Starting at offset: %u\n", offset);)
+
+                while(bytes_to_dump > 0)
                 {
                     unsigned int num_read;
-                    
-                    num_read = fread( buffer, sizeof(unsigned char), buffer_size, fp );
-                    
-                    DBGCODE( printf( "fread() returned %d\n", num_read ); )
-                    
-                    if( num_read == 0 )
+
+                    num_read = fread(buffer, sizeof(unsigned char), buffer_size, fp);
+
+                    DBGCODE(printf("fread() returned %d\n", num_read);)
+
+                    if(num_read == 0)
                     {
                         bytes_to_dump = 0L;
                         break;
                     }
-                    
+
                     unsigned char *cur = buffer;
-                    
+
                     bytes_to_dump -= num_read;
-                    
-                    while( num_read > 0 )
+
+                    while(num_read > 0)
                     {
-                        unsigned int count = (unsigned int) ( ( num_read > 15 ) ? 16 : num_read );
-                        
-                        format_hex( str, flags, offset, cur, count );
-                        
-                        fprintf( stdout, "%s\n", str );
-                        
+                        unsigned int count = (unsigned int)((num_read > 15) ? 16 : num_read);
+
+                        format_hex(str, flags, offset, cur, count);
+
+                        fprintf(stdout, "%s\n", str);
+
                         num_read -= count;
-                        
+
                         cur += count;
-                        
+
                         offset += count;
                     }
                 }
-                
+
                 delete [] str;
             }
             else
             {
-                DBGCODE( fprintf( stderr, "new(%d) failed\n", STRSIZE ); )
+                DBGCODE(fprintf(stderr, "new(%d) failed\n", STRSIZE);)
             }
-            
+
             delete [] buffer;
         }
         else
         {
-            DBGCODE( printf( "new(%d) failed\n", buffer_size ); )
+            DBGCODE(printf("new(%d) failed\n", buffer_size);)
         }
-        
-        DBGCODE( int ret = )
 
-        fclose( fp );
+        DBGCODE(int ret =)
+
+        fclose(fp);
 
         DBGCODE(
-            if( ret != 0 )
-            {
-                fprintf( stderr, "error closing file\n" );
-            }
+            if(ret != 0)
+    {
+        fprintf(stderr, "error closing file\n");
+        }
         )
     }
-    
+
     return true;    // to continue processing.
 }
 
 
 
 
-char *format_hex( char *str, unsigned int flags,
-                  unsigned int offset, unsigned char const *buffer, unsigned int count )
+char *format_hex(char *str, unsigned int flags,
+                 unsigned int offset, unsigned char const *buffer, unsigned int count)
 {
     char hex_str[ 60 ] = { 0 };
     char ascii_string[ 20 ];
@@ -341,71 +341,71 @@ char *format_hex( char *str, unsigned int flags,
     unsigned char b;
     unsigned int i;
 
-    
-    for( i = 0; i < count; i++ )
+
+    for(i = 0; i < count; i++)
     {
-        b = *( buffer + i );
-        
-        sprintf( temp_str, "%02x ", b );
-        
-        strcat( hex_str, temp_str );
-        
-        if( i == 7 )
+        b = *(buffer + i);
+
+        sprintf(temp_str, "%02x ", b);
+
+        strcat(hex_str, temp_str);
+
+        if(i == 7)
         {
-            strcat( hex_str, " " );
+            strcat(hex_str, " ");
         }
-        
-        if( flags & DF_SHOW_ASCII )
+
+        if(flags & DF_SHOW_ASCII)
         {
-            ascii_string[ i ] = AsciiFromBin( b );
+            ascii_string[ i ] = AsciiFromBin(b);
         }
         else
         {
             ascii_string[ i ] = 0;
         }
     }
-    
+
     ascii_string[ i ] = 0;
-    
-    if( flags & DF_SHOW_ADDRESS )
+
+    if(flags & DF_SHOW_ADDRESS)
     {
-        if( flags & DF_SEGMENTED_ADDRESS )
+        if(flags & DF_SEGMENTED_ADDRESS)
         {
-            sprintf( str, "%04x:%04x   ", hi_word( offset ), lo_word( offset ) );
+            sprintf(str, "%04x:%04x   ", hi_word(offset), lo_word(offset));
         }
         else
         {
-            sprintf( str, "%08x   ", offset );
+            sprintf(str, "%08x   ", offset);
         }
     }
     else
     {
         // No Addresses.
-        
+
         *str = 0;
     }
-    
-    
-    while ( strlen( hex_str ) < 49 )
+
+
+    while(strlen(hex_str) < 49)
     {
-        strcat( hex_str, " " );
+        strcat(hex_str, " ");
     }
-    
-    
+
+
     // Add the hex output to the result string.
-    
-    strcat( str, hex_str );
-    
-    
-    if ( flags & DF_SHOW_ASCII )
+
+    strcat(str, hex_str);
+
+
+    if(flags & DF_SHOW_ASCII)
     {
         // Leave two blank spaces.
-        
-        strcat( str, "  " );
-        
-        strcat( str, ascii_string );
+
+        strcat(str, "  ");
+
+        strcat(str, ascii_string);
     }
-    
+
     return str;
 }
 
@@ -414,21 +414,21 @@ char *format_hex( char *str, unsigned int flags,
 
 #ifdef _FILELENGTH
 
-unsigned int file_length( FILE *fp )
+unsigned int file_length(FILE *fp)
 {
     unsigned int len(0);
 
     // Remember the current position in the file.
-    unsigned int cur = ftell( fp );
+    unsigned int cur = ftell(fp);
 
     // Seek to the end of the file.
-    fseek( fp, 0, SEEK_END );
+    fseek(fp, 0, SEEK_END);
 
     // Take the current position of the file (the end) as the size of the file.
-    len = ftell( fp );
+    len = ftell(fp);
 
     // Return the file position to the initial location.
-    fseek( fp, cur, SEEK_SET );
+    fseek(fp, cur, SEEK_SET);
 
     return len;
 }
@@ -439,14 +439,14 @@ unsigned int file_length( FILE *fp )
 
 
 
-void display_text( const char **str )
+void display_text(const char **str)
 {
-    const char **p( str );
-    
-    while( p != 0 && *p != 0 )
+    const char **p(str);
+
+    while(p != 0 && *p != 0)
     {
-        fprintf( stderr, "%s\n", *p );
-        
+        fprintf(stderr, "%s\n", *p);
+
         p++;
     }
 }
